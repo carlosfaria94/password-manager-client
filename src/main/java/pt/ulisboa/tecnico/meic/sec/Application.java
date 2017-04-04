79a1211a2083d73ae532ca747a6964669b1c6ede
Francisco Santos <franciscopolaco@ist.utl.pt>
2017-04-04 18:51:29 +0100

    interactive shell. Gotta have a big heart

--- src/main/java/pt/ulisboa/tecnico/meic/sec/Application.java
+++ src/main/java/pt/ulisboa/tecnico/meic/sec/Application.java
@@ -1,49 +1,102 @@
 package pt.ulisboa.tecnico.meic.sec;
 
 import pt.ulisboa.tecnico.meic.sec.lib.PwdManagerClient;
+import pt.ulisboa.tecnico.meic.sec.lib.exception.RemoteServerInvalidResponseException;
+import pt.ulisboa.tecnico.meic.sec.lib.exception.ServersIntegrityException;
+import pt.ulisboa.tecnico.meic.sec.lib.exception.ServersSignatureNotValidException;
 
 import java.io.Console;
+import java.io.IOException;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args){
-        PwdManagerClient client = new PwdManagerClient();
-        System.out.println(client.helloWorld());
+    private static final String PASSWORD = "batata";
 
-        if(args.length != 1) {
+    public static void main(String[] args)
+            throws CertificateException, NoSuchAlgorithmException, KeyStoreException,
+            IOException, RemoteServerInvalidResponseException, ServersIntegrityException,
+            ServersSignatureNotValidException {
+
+        String ksPath;
+        if (args.length != 1) {
             System.err.println("Argument(s) missing!");
             System.err.printf("Usage: java %s keystorePath%n", Application.class.getName());
-            return;
-        }
-        Console console = System.console();
+            System.err.println("Assuming default path.");
+            ksPath = "keystore.jceks";
+        }else
+            ksPath = args[0];
 
-        System.out.println("Keystore Password:");
-        char[] password = console.readPassword(); // batata
+        PwdManagerClient client = new PwdManagerClient();
 
-        try {
-            KeyStore ks = CryptoUtilities.readKeystoreFile(args[0], password);
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Asymmetric Key Alias:");
-            String asymmAlias = scanner.nextLine();
-            System.out.println("Asymmetric Key Password:");
-            char[] asymmPassword = console.readPassword(); // batata
-            System.out.println("Symmetric Key Alias:");
-            String symmAlias = scanner.nextLine();
-            System.out.println("Symmetric Key Password:");
-            char[] symmPassword = console.readPassword(); // batata
+        final KeyStore keyStore = CryptoUtilities.readKeystoreFile(ksPath, PASSWORD.toCharArray());
+        client.init(keyStore, "asymm", PASSWORD.toCharArray(), "symm", PASSWORD.toCharArray());
 
-            client.init(ks, asymmAlias, asymmPassword, symmAlias, symmPassword);
-            client.register_user();
-            client.save_password("youtube.com", "batata", "batata123");
-            System.out.println(client.retrieve_password("youtube.com", "batata"));
-            client.save_password("youtube.com", "batata", "batata123");
-            System.out.println(client.retrieve_password("youtube.com", "batata"));
-            client.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.getMessage());
-            System.exit(0);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("\n\nMenu\n" +
+                    "0 - Exit\n" +
+                    "1 - Register\n" +
+                    "2 - Save Password\n" +
+                    "3 - Retrieve Password\n" +
+                    "4 - Hello\n" +
+                    "\n> ");
+            int option;
+            try {
+                option = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                System.err.println("Not a number.");
+                continue;
+            }
+            scanner.nextLine();
+
+
+            if (option == 0) {
+                System.out.println("Exiting...");
+                client.close();
+                break;
+            }
+            switch (option) {
+                // Register
+                case 1:
+                    System.out.println("Registering user...");
+                    client.register_user();
+                    break;
+                //Save Pwd
+                case 2:
+                    System.out.println("Fill the following fields:");
+                    System.out.print("Domain: ");
+                    String domain = scanner.nextLine();
+                    System.out.print("Username: ");
+                    String username = scanner.nextLine();
+                    System.out.print("Password: ");
+                    String pwd = scanner.nextLine();
+                    client.save_password(domain, username, pwd);
+                    break;
+                // retrieve
+                case 3:
+                    System.out.println("Fill the following fields:");
+                    System.out.print("Domain: ");
+                    String domain2 = scanner.nextLine();
+                    System.out.print("Username: ");
+                    String username2 = scanner.nextLine();
+                    System.out.println("Password retrieved: " + client.retrieve_password(domain2, username2));
+                    break;
+
+                case 4:
+                    System.out.println(client.helloWorld());
+                    break;
+
+                default:
+                    System.out.println("No puedo!");
+                    break;
+            }
+
         }
     }
 }
